608f96d1ad001b5e9e41a3e589987092d0f1396d
bielsnohr <matthew.bluteau@gmail.com>
2021-12-13 01:16:01 +0000

    Solution for Puzzle 2 of Day 2

--- 02/dive.py
+++ 02/dive.py
@@ -22,6 +22,22 @@ class Submarine():
         return self.hpos * self.depth
 
 
+class AimSubmarine(Submarine):
+    def __init__(self):
+        super().__init__()
+        self.aim = 0
+
+    def forward(self, units):
+        self.hpos += units
+        self.depth += self.aim * units
+
+    def up(self, units):
+        self.aim -= units
+
+    def down(self, units):
+        self.aim += units
+
+
 def puzzle_1(inputfile: Path) -> int:
     aoc_sub = Submarine()
     with open(inputfile, 'r') as file:
@@ -32,13 +48,18 @@ def puzzle_1(inputfile: Path) -> int:
 
 
 def puzzle_2(inputfile: Path) -> int:
-    pass
+    aoc_sub = AimSubmarine()
+    with open(inputfile, 'r') as file:
+        for line in file:
+            instruction, units = line.split()
+            aoc_sub.execute_instruction(instruction, int(units))
+    return aoc_sub.multiply_positions()
 
 
 def main():
     inputfile = Path("puzzle_1_input.txt")
     print(f"Puzzle 1 Answer = {puzzle_1(inputfile)}")
-    #print(f"Puzzle 2 Answer = {puzzle_2(inputfile)}")
+    print(f"Puzzle 2 Answer = {puzzle_2(inputfile)}")
 
 
 if __name__ == "__main__":
--- 02/test_dive.py
+++ 02/test_dive.py
@@ -1,4 +1,5 @@
 from dive import Submarine, puzzle_1, Path
+from dive import AimSubmarine, puzzle_2
 import pytest
 
 
@@ -7,5 +8,26 @@ def submarine():
     return Submarine()
 
 
-def test_puzzle_1(submarine):
+@pytest.fixture
+def aimsub():
+    return AimSubmarine()
+
+
+def test_puzzle_1():
     assert puzzle_1(Path(__file__).parent / "puzzle_1_test_input.txt") == 150
+
+
+def test_aimsub_forward(aimsub):
+    aimsub.aim = 10
+    aimsub.forward(10)
+    assert (aimsub.hpos, aimsub.depth) == (10, 100)
+
+
+def test_aimsub_up_down_forward(aimsub):
+    aimsub.down(10)
+    aimsub.up(5)
+    aimsub.forward(10)
+    assert (aimsub.hpos, aimsub.depth) == (10, 50)
+
+def test_puzzle_2():
+    assert puzzle_2(Path(__file__).parent / "puzzle_1_test_input.txt") == 900
