1e82b52c7cbd64ba17517b06f00a94da80fe840f
bielsnohr <matthew.bluteau@gmail.com>
2021-12-30 18:17:05 -0400

    Solve puzzle 2 of day 7

--- 07/test_whale_treachery.py
+++ 07/test_whale_treachery.py
@@ -1,4 +1,4 @@
-from whale_treachery import calculate_fuel
+from whale_treachery import calculate_fuel, abs_diff, fuel_cost, arithmetic_sum, minimise_fuel_cost
 from whale_treachery import puzzle_1, puzzle_2, Path
 import pytest
 
@@ -14,6 +14,20 @@ def test_calculate_fuel(crab_positions):
 
 def test_puzzle_1():
     assert puzzle_1(Path(__file__).parent / "puzzle_1_test_input.txt") == 37
+
+
+def test_fuel_cost(crab_positions):
+    assert fuel_cost(2, crab_positions, abs_diff) == 37
+    assert fuel_cost(5, crab_positions, arithmetic_sum) == 168
+
+
+def test_arithmetic_sum():
+    assert arithmetic_sum(16, 5) == 66
+
+
+def test_minimise_fuel_cost(crab_positions):
+    assert minimise_fuel_cost(crab_positions, abs_diff) == (37, 2)
+    assert minimise_fuel_cost(crab_positions, arithmetic_sum) == (168, 5)
 
 # def test_puzzle_2():
 #     assert puzzle_2(Path(__file__).parent / "puzzle_1_test_input.txt") == 26984457539
--- 07/whale_treachery.py
+++ 07/whale_treachery.py
@@ -1,17 +1,49 @@
 #! /usr/bin/env python3
 from pathlib import Path
+from typing import Callable, Tuple
 import numpy.typing as npt
 import numpy as np
-
-
-def calculate_median(crab_positions: npt.ArrayLike) -> int:
-    return np.floor(np.median(crab_positions))
+import scipy.optimize as opt
 
 
 def calculate_fuel(crab_positions: npt.ArrayLike) -> int:
     positions = np.asarray(crab_positions)
     median = np.floor(np.median(positions))
     return np.sum(np.abs(positions - median))
+
+
+def fuel_cost(position: int, crab_positions: npt.ArrayLike, distance_function: Callable) -> int:
+    positions = np.asarray(crab_positions)
+    dist_func = np.vectorize(distance_function, otypes=[np.int64])
+    return np.sum(dist_func(positions, position))
+
+
+def abs_diff(a, b):
+    return np.int64(np.abs(a - b))
+
+
+def arithmetic_sum(a, b):
+    n = np.abs(a - b)
+    return np.int64(n * (n + 1) / 2)
+
+
+def minimise_fuel_cost(crab_positions: npt.ArrayLike, distance_function: Callable) -> Tuple[int, int]:
+    def f(position):
+        return fuel_cost(position, crab_positions, distance_function)
+    positions = np.asarray(crab_positions)
+    mid = np.floor(np.median(positions))
+    costs = [f(mid-1), f(mid), f(mid+1)]
+    while True:
+        if costs[1] < costs[0] and costs[1] < costs[2]:
+            return (costs[1], mid)
+        elif costs[0] < costs[1]:
+            mid -= 1
+            costs.pop()
+            costs.insert(0, f(mid-1))
+        elif costs[2] < costs[1]:
+            mid += 1
+            costs.pop(0)
+            costs.append(f(mid+1))
 
 
 def puzzle_1(inputfile: Path, diagonal=False) -> int:
@@ -21,17 +53,15 @@ def puzzle_1(inputfile: Path, diagonal=False) -> int:
 
 
 def puzzle_2(inputfile: Path) -> int:
-    pass
-    # with open(inputfile, 'r') as file:
-    #     initial_state = [int(x) for x in file.readline().strip('\n').split(sep=',')]
-    # tracker = LanternfishTracker(timer_list=initial_state)
-    # return tracker.simulate_growth(days=256)
+    with open(inputfile, 'r') as file:
+        crab_positions = [int(x) for x in file.readline().strip('\n').split(sep=',')]
+    return minimise_fuel_cost(crab_positions=crab_positions, distance_function=arithmetic_sum)[0]
 
 
 def main():
     inputfile = Path("puzzle_1_input.txt")
     print(f"Puzzle 1 Answer = {puzzle_1(inputfile)}")
-    #print(f"Puzzle 2 Answer = {puzzle_2(inputfile)}")
+    print(f"Puzzle 2 Answer = {puzzle_2(inputfile)}")
 
 
 if __name__ == "__main__":
